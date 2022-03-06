8d8b8270b1d1f9ffb4edff5e3e6bb860e3a4ae58
munaabuh <munaabuh@gmail.com>
2022-03-06 16:43:01 +0200

    started the process of detail Activity for for thr about us page

--- app/src/main/java/com/example/myapplication/AboutUsActivity.java
+++ app/src/main/java/com/example/myapplication/AboutUsActivity.java
@@ -1,15 +1,30 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class AboutUsActivity extends AppCompatActivity {
+public class AboutUsActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
+    private CardView missionAndValues,medicalExpertise,privacyAndSecurity,membership;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        missionAndValues= findViewById(R.id.missionAndValues);
+        medicalExpertise= findViewById(R.id.medicalExpertise);
+        privacyAndSecurity= findViewById(R.id.privacyAndSecurity);
+        membership= findViewById(R.id.membership);
+
+
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+
     }
 }
